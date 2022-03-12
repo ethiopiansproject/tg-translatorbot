fdc304e0fc030ccdf4d87e5ee9b1f780f9de565a
ethiopiansproject <101466524+ethiopiansproject@users.noreply.github.com>
2022-03-12 20:35:02 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram import*
 from telegram.ext import*
 from googletrans import Translator
 
-api_key=os.getenv('BOT_TOKEN')
+api_key=os.getenv(BOT_TOKEN)
 def start(update,context):
  user_info = update.effective_user
  update.message.reply_markdown_v2(f'ሠላም ውድ {user_info.mention_markdown_v2()} የተለያዩ ቋንቋዎችን ወደ አማርኛ መቶርጎም እችላለው😊\nየፈለጋችሁትን ፅፈት ላኩልኝ፣መልካም ግዜ🤗')
@@ -19,7 +19,7 @@ def translate_text(update,context):
 <b>{translation.text}</b> 
 <code>\nPowered by @The_ep </code>''',quote=True)
    
-updater = Updater('5171615033:AAEi0-vaZjzz-sUXI_7jmJU5OSECi95J-Ag')
+updater = Updater(api_key)
 
 dp = updater.dispatcher
 dp.add_handler(CommandHandler('start',start))
